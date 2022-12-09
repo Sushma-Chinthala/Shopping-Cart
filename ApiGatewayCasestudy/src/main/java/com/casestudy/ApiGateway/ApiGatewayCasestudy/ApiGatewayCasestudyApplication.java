2db09e92366c1fde16183834a8db620f9355819b
Sushma-Chinthala <115689413+Sushma-Chinthala@users.noreply.github.com>
2022-12-09 19:01:45 +0530

    Add files via upload

---
+++ ApiGatewayCasestudy/src/main/java/com/casestudy/ApiGateway/ApiGatewayCasestudy/ApiGatewayCasestudyApplication.java
@@ -0,0 +1,13 @@
+package com.casestudy.ApiGateway.ApiGatewayCasestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayCasestudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayCasestudyApplication.class, args);
+	}
+
+}
---
+++ ApiGatewayCasestudy/src/test/java/com/casestudy/ApiGateway/ApiGatewayCasestudy/ApiGatewayCasestudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casestudy.ApiGateway.ApiGatewayCasestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayCasestudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
